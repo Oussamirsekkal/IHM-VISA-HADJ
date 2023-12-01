b037cc075980496bc5639ed4dcd318bb61765e7c
Oussamirsekkal <142433285+Oussamirsekkal@users.noreply.github.com>
2023-12-01 23:51:42 +0100

    hello

--- app/footer.tsx
+++ app/footer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white py-8">
+    <footer className="bg-emerald-800 text-white py-8">
       <div className="container mx-auto flex flex-col md:flex-row md:justify-between items-center">
         <div className="text-center md:text-left mb-4 md:mb-0">
           <p className="mb-2 font-bold">Sekkal Oussama Amir:</p>
--- app/page.tsx
+++ app/page.tsx
@@ -17,6 +17,7 @@ const Home = () => {
 
 
   const handleNext = () => {
+    
     setCurrentForm((prev) => prev + 1);
   };
   const handleprev = () => {
