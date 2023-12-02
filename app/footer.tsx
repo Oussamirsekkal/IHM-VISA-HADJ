2ca0f080c6c9fda7c00771af1684a45f64e1e4cc
Oussamirsekkal <142433285+Oussamirsekkal@users.noreply.github.com>
2023-12-02 15:13:05 +0100

    hello

--- app/footer.tsx
+++ app/footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="bg-emerald-800 text-white py-8">
       <div className="container mx-auto flex flex-col md:flex-row md:justify-between items-center">
         <div className="text-center md:text-left mb-4 md:mb-0">
-          <p className="mb-2 font-bold">Sekkal Oussama Amir:</p>
+          <p className="mb-2 font-bold">Sekkal Oussama Amir</p>
           <div className="flex justify-center md:justify-start space-x-4">
             <Link href="https://www.facebook.com/ousssekkal.amir/" target="_blank">
               <Image src="/fb.png" alt="Facebook" width={20} height={20} />
@@ -20,7 +20,7 @@ const Footer = () => {
           </div>
         </div>
         <div className="text-center md:text-left">
-          <p className="mb-2 font-bold">Makhlouf Ibrahim:</p>
+          <p className="mb-2 font-bold">Makhlouf Ibrahim</p>
           <div className="flex justify-center md:justify-start space-x-4">
             <Link href="https://www.facebook.com/profile.php?id=100017719095409" target="_blank">
               <Image src="/fb.png" alt="Facebook" width={20} height={20} />
