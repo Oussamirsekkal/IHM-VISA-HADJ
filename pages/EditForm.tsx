3c8cd6f13e2cf48d42ccff8380be0381aef9d50a
Oussamirsekkal <142433285+Oussamirsekkal@users.noreply.github.com>
2023-12-09 21:48:44 +0100

    update 09/12/2023 amin panel is working edit/delete 9:45 pm

--- pages/EditForm.tsx
+++ pages/EditForm.tsx
@@ -138,14 +138,14 @@ const EditForm: React.FC<EditFormProps> = ({ rowData, onCancel, onSave }) => {
         
           <label className="block mt-4">
   <span className="text-gray-700">First Name:</span>
-  <input
+  {rowData && rowData.first_name && ( <input
     type="text"
     name="first_name"
     value={editedData.first_name}
     pattern="[A-Za-z]+"
     onChange={handleInputChange}
     className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-  />
+  />)}
 </label>
 
           <label className="block mt-4">
